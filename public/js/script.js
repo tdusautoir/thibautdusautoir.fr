3ff5d3d5dfc1e83c24d3480615f7647317110388
Thibaut Dusautoir <tdusautoir@myges.fr>
2023-01-30 23:16:22 +0100

    homepage - animation header, and init section profile

---
+++ public/js/script.js
@@ -0,0 +1,67 @@
+/* CONST */
+
+const header = document.querySelector('header');
+const headerNavLinks = document.getElementsByClassName('nav__link');
+const sections = document.querySelectorAll('section')
+
+/* HEADER */
+
+window.addEventListener(
+    "scroll",
+    function () {
+        header.classList.toggle("header--sticky", window.scrollY > 40);
+    }
+);
+
+/* header opacity animation */
+for(let i = 0; i < headerNavLinks.length; i++){
+    headerNavLinks[i].addEventListener('mouseover', (e) => {
+        for(let j = 0; j < headerNavLinks.length; j++){
+            headerNavLinks[j].classList.add('disable');
+            headerNavLinks[j].classList.remove('active');
+        }
+
+        e.target.classList.remove('disable');
+        e.target.classList.add('active');
+    })
+
+    headerNavLinks[i].addEventListener('mouseleave', (e) => {
+        for(let j = 0; j < headerNavLinks.length; j++){
+            headerNavLinks[j].classList.remove('disable');
+            headerNavLinks[j].classList.remove('active');
+        }
+    })
+}
+
+/* OBSERVER */
+
+let observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if(!entry.isIntersecting){
+            entry.target.classList.add('active');
+            for(let i = 0; i < headerNavLinks.length; i++){
+              if((headerNavLinks[i].getAttribute('href') == `#${entry.target.getAttribute('id')}`)){
+                headerNavLinks[i].classList.add('unseen');
+              }
+            }
+        } else {
+            entry.target.classList.remove('active');
+            for(let i = 0; i < headerNavLinks.length; i++){
+              if((headerNavLinks[i].getAttribute('href') == `#${entry.target.getAttribute('id')}`)){
+                headerNavLinks[i].classList.remove('unseen');
+              }
+            }
+        }
+      });
+    },
+    {
+      threshold: 0.2,
+    }
+);
+
+sections.forEach(observed => {
+    if(observed){
+        observer.observe(observed);
+    }
+});
