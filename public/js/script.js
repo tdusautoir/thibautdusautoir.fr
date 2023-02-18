d74cb8da9201e7ea15a3906f4c35449b6671ba95
Thibaut Dusautoir <tdusautoir@myges.fr>
2023-02-18 16:23:25 +0100

    portfolio - add filter

--- public/js/script.js
+++ public/js/script.js
@@ -18,53 +18,61 @@ window.addEventListener(
 );
 
 header.classList.toggle("unseen", window.scrollY > 40);
-headerNav.classList.toggle("header__nav--sticky", window.scrollY > 40);
+if(headerNav) {
+  headerNav.classList.toggle("header__nav--sticky", window.scrollY > 40);
+}
 
 /* header opacity animation */
-for(let i = 0; i < headerNavLinks.length; i++){
-    headerNavLinks[i].addEventListener('mouseover', (e) => {
-        for(let j = 0; j < headerNavLinks.length; j++){
-            headerNavLinks[j].classList.add('disable');
-            headerNavLinks[j].classList.remove('active');
-        }
-
-        headerNavLinks[i].classList.remove('disable');
-        headerNavLinks[i].classList.add('active');
-    })
-
-    headerNavLinks[i].addEventListener('mouseleave', (e) => {
-        for(let j = 0; j < headerNavLinks.length; j++){
-            headerNavLinks[j].classList.remove('disable');
-            headerNavLinks[j].classList.remove('active');
-        }
-    })
+if(headerNavLinks.length > 0) {
+  for(let i = 0; i < headerNavLinks.length; i++){
+      headerNavLinks[i].addEventListener('mouseover', (e) => {
+          for(let j = 0; j < headerNavLinks.length; j++){
+              headerNavLinks[j].classList.add('disable');
+              headerNavLinks[j].classList.remove('active');
+          }
+  
+          headerNavLinks[i].classList.remove('disable');
+          headerNavLinks[i].classList.add('active');
+      })
+  
+      headerNavLinks[i].addEventListener('mouseleave', (e) => {
+          for(let j = 0; j < headerNavLinks.length; j++){
+              headerNavLinks[j].classList.remove('disable');
+              headerNavLinks[j].classList.remove('active');
+          }
+      })
+  }
 }
 
 /* OBSERVER */
 
 let observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if(!entry.isIntersecting){
-            entry.target.classList.add('active');
-            for(let i = 0; i < headerNavLinks.length; i++){
-              if((headerNavLinks[i].getAttribute('href') == `#${entry.target.getAttribute('id')}`)){
-                headerNavLinks[i].classList.add('unseen');
-              }
+  (entries) => {
+    entries.forEach((entry) => {
+      if(!entry.isIntersecting){
+        entry.target.classList.add('active');
+        if(headerNavLinks.length > 0) {
+          for(let i = 0; i < headerNavLinks.length; i++){
+            if((headerNavLinks[i].getAttribute('href') == `#${entry.target.getAttribute('id')}`)){
+              headerNavLinks[i].classList.add('unseen');
             }
-        } else {
-            entry.target.classList.remove('active');
-            for(let i = 0; i < headerNavLinks.length; i++){
-              if((headerNavLinks[i].getAttribute('href') == `#${entry.target.getAttribute('id')}`)){
-                headerNavLinks[i].classList.remove('unseen');
-              }
-            }
+          }
         }
-      });
-    },
-    {
-      threshold: 0.2,
-    }
+      } else {
+        entry.target.classList.remove('active');
+        if(headerNavLinks.length > 0) {
+          for(let i = 0; i < headerNavLinks.length; i++){
+            if((headerNavLinks[i].getAttribute('href') == `#${entry.target.getAttribute('id')}`)){
+              headerNavLinks[i].classList.remove('unseen');
+            }
+          }
+        }
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
 );
 
 sections.forEach(observed => {
@@ -74,7 +82,6 @@ sections.forEach(observed => {
 });
 
 /* DARKMODE */
-
 
 var theme = localStorage.getItem('theme');
 if(theme) {
@@ -98,3 +105,21 @@ nightmodeBtn.addEventListener('click', () => {
   document.body.classList.remove('night');
   localStorage.setItem('theme', 'light');
 });
+
+/* PORTFOLIO */
+
+const filter_btns = document.querySelectorAll('.filters__btn');
+const portfolio_cards = document.querySelectorAll('.portfolio-container .card');
+
+for(var i = 0; i < filter_btns.length; i++) {
+  filter_btns[i].addEventListener('click', (e) => {
+    console.log(e.target.dataset.filter);
+    for(var i = 0; i < portfolio_cards.length; i++) {
+      if(JSON.parse(portfolio_cards[i].dataset.stack.includes(e.target.dataset.filter))) {
+        portfolio_cards[i].style.display = "block";
+      } else {
+        portfolio_cards[i].style.display = "none";
+      }
+    }
+  })
+}
