79f03749fe333c3864aedaec777deda77c274e4c
Thibaut Dusautoir <tdusautoir@myges.fr>
2023-02-24 22:24:26 +0100

    nav - add animation underline

--- public/js/script.js
+++ public/js/script.js
@@ -1,72 +1,81 @@
-/* CONST */
-
-const header = document.querySelector('header');
-const headerNav = document.querySelector('.header__nav');
-const headerNavLinks = document.getElementsByClassName('nav__link');
-const sections = document.querySelectorAll('section');
-const lightmodeBtn = document.getElementById('lightmode_btn');
-const nightmodeBtn = document.getElementById('nightmode_btn');
-
 /* HEADER */
 
-window.addEventListener(
-    "scroll",
-    function () {
-        header.classList.toggle("unseen", window.scrollY > 40);
-        if(headerNav) {
-          headerNav.classList.toggle("header__nav--sticky", window.scrollY > 40);
-        }
-    }
-);
+const header = document.querySelector("header");
+const headerNav = document.querySelector(".header__nav");
+const headerNavLinkContainer = document.querySelector(".header__nav__link__container");
+const headerNavHighlight = document.getElementById('nav-highlight');
+const headerNavLinks = document.getElementsByClassName("nav__link");
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("unseen", window.scrollY > 40);
+  if (headerNav) {
+    headerNav.classList.toggle("header__nav--sticky", window.scrollY > 40);
+  }
+});
 
 header.classList.toggle("unseen", window.scrollY > 40);
-if(headerNav) {
+if (headerNav) {
   headerNav.classList.toggle("header__nav--sticky", window.scrollY > 40);
 }
 
 /* header opacity animation */
-if(headerNavLinks.length > 0) {
-  for(let i = 0; i < headerNavLinks.length; i++){
-      headerNavLinks[i].addEventListener('mouseover', (e) => {
-          for(let j = 0; j < headerNavLinks.length; j++){
-              headerNavLinks[j].classList.add('disable');
-              headerNavLinks[j].classList.remove('active');
-          }
-  
-          headerNavLinks[i].classList.remove('disable');
-          headerNavLinks[i].classList.add('active');
-      })
-  
-      headerNavLinks[i].addEventListener('mouseleave', (e) => {
-          for(let j = 0; j < headerNavLinks.length; j++){
-              headerNavLinks[j].classList.remove('disable');
-              headerNavLinks[j].classList.remove('active');
-          }
-      })
+if (headerNavLinks.length > 0) {
+  for (let i = 0; i < headerNavLinks.length; i++) {
+    headerNavLinks[i].addEventListener("mouseover", (e) => {
+      headerNavHighlight.style.left = (e.currentTarget.getBoundingClientRect().left - headerNavLinkContainer.getBoundingClientRect().left) + 'px';
+      headerNavHighlight.style.width = (e.currentTarget.getBoundingClientRect().width) + 'px';
+
+      for (let j = 0; j < headerNavLinks.length; j++) {
+        headerNavLinks[j].classList.add("disable");
+        headerNavLinks[j].classList.remove("active");
+      }
+
+      e.currentTarget.classList.remove("disable");
+      e.currentTarget.classList.add("active");
+    });
+
+    headerNavLinks[i].addEventListener("mouseleave", () => {
+      for (let j = 0; j < headerNavLinks.length; j++) {
+        headerNavLinks[j].classList.remove("disable");
+        headerNavLinks[j].classList.remove("active");
+      }
+    });
   }
+
+  headerNavLinkContainer.addEventListener("mouseleave", () => {
+    headerNavHighlight.style.width = '0px';
+  });
 }
 
 /* OBSERVER */
 
+const sections = document.querySelectorAll("section");
+
 let observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      if(!(entry.target.classList.contains('project'))) { 
-        if(!entry.isIntersecting) {
-          entry.target.classList.add('active');
-          if(headerNavLinks.length > 0) {
-            for(let i = 0; i < headerNavLinks.length; i++){
-              if((headerNavLinks[i].getAttribute('href') == `#${entry.target.getAttribute('id')}`)){
-                headerNavLinks[i].classList.add('unseen');
+      if (!entry.target.classList.contains("project")) {
+        if (!entry.isIntersecting) {
+          entry.target.classList.add("active");
+          if (headerNavLinks.length > 0) {
+            for (let i = 0; i < headerNavLinks.length; i++) {
+              if (
+                headerNavLinks[i].getAttribute("href") ==
+                `#${entry.target.getAttribute("id")}`
+              ) {
+                headerNavLinks[i].classList.add("unseen");
               }
             }
           }
         } else {
-          entry.target.classList.remove('active');
-          if(headerNavLinks.length > 0) {
-            for(let i = 0; i < headerNavLinks.length; i++){
-              if((headerNavLinks[i].getAttribute('href') == `#${entry.target.getAttribute('id')}`)){
-                headerNavLinks[i].classList.remove('unseen');
+          entry.target.classList.remove("active");
+          if (headerNavLinks.length > 0) {
+            for (let i = 0; i < headerNavLinks.length; i++) {
+              if (
+                headerNavLinks[i].getAttribute("href") ==
+                `#${entry.target.getAttribute("id")}`
+              ) {
+                headerNavLinks[i].classList.remove("unseen");
               }
             }
           }
@@ -79,92 +88,103 @@ let observer = new IntersectionObserver(
   }
 );
 
-sections.forEach(observed => {
-    if(observed){
-        observer.observe(observed);
-    }
+sections.forEach((observed) => {
+  if (observed) {
+    observer.observe(observed);
+  }
 });
 
 /* DARKMODE */
 
-var theme = localStorage.getItem('theme');
-if(theme) {
-  var css = "*{transition: none!important}";
-  var style = document.createElement('style');
+const lightmodeBtn = document.getElementById("lightmode_btn");
+const nightmodeBtn = document.getElementById("nightmode_btn");
 
-  style.id = 'transition-none';
+var theme = localStorage.getItem("theme");
+if (theme) {
+  var css = "*{transition: none!important}";
+  var style = document.createElement("style");
+
+  style.id = "transition-none";
   style.appendChild(document.createTextNode(css));
   document.head.appendChild(style);
-  theme === 'night' ? document.body.classList.add('night') : document.body.classList.remove('night');
-  
-  setTimeout(() => {document.getElementById('transition-none').remove()}, 100);
+  theme === "night"
+    ? document.body.classList.add("night")
+    : document.body.classList.remove("night");
+
+  setTimeout(() => {
+    document.getElementById("transition-none").remove();
+  }, 100);
 }
 
-lightmodeBtn.addEventListener('click', () => {
-  document.body.classList.add('night');
-  localStorage.setItem('theme', 'night');
+lightmodeBtn.addEventListener("click", () => {
+  document.body.classList.add("night");
+  localStorage.setItem("theme", "night");
 });
 
-nightmodeBtn.addEventListener('click', () => {
-  document.body.classList.remove('night');
-  localStorage.setItem('theme', 'light');
+nightmodeBtn.addEventListener("click", () => {
+  document.body.classList.remove("night");
+  localStorage.setItem("theme", "light");
 });
 
 /* PORTFOLIO */
 
-const filter_btns = document.querySelectorAll('.filters__btn');
-const portfolio_cards = document.querySelectorAll('.portfolio-container .card');
-const portfolio_cards_btn = document.querySelectorAll('.portfolio-container .card button');
-const project_sections = document.querySelectorAll('section.project');
+const filter_btns = document.querySelectorAll(".filters__btn");
+const portfolio_cards = document.querySelectorAll(".portfolio-container .card");
+const portfolio_cards_btn = document.querySelectorAll(".portfolio-container .card button");
+const project_sections = document.querySelectorAll("section.project");
 
-for(let i = 0; i < filter_btns.length; i++) {
-  filter_btns[i].addEventListener('click', (e) => {
-    if(e.target.classList.contains('selected')) {
-      for(let j = 0; j < portfolio_cards.length; j++) {
-        portfolio_cards[j].classList.remove('notshow');
+for (let i = 0; i < filter_btns.length; i++) {
+  filter_btns[i].addEventListener("click", (e) => {
+    if (e.target.classList.contains("selected")) {
+      for (let j = 0; j < portfolio_cards.length; j++) {
+        portfolio_cards[j].classList.remove("notshow");
       }
 
-      e.target.classList.remove('selected');
+      e.target.classList.remove("selected");
     } else {
-      for(let j = 0; j < portfolio_cards.length; j++) {
+      for (let j = 0; j < portfolio_cards.length; j++) {
         let portfolio_card = portfolio_cards[j];
-        if(JSON.parse(portfolio_card.dataset.stack.includes(e.target.dataset.filter))) {
-          portfolio_card.classList.remove('notshow');
+        if (
+          JSON.parse(
+            portfolio_card.dataset.stack.includes(e.target.dataset.filter)
+          )
+        ) {
+          portfolio_card.classList.remove("notshow");
         } else {
-          portfolio_card.classList.add('notshow');
+          portfolio_card.classList.add("notshow");
         }
       }
 
-      for(let j = 0; j < filter_btns.length; j++) {
-        filter_btns[j].classList.remove('selected');
+      for (let j = 0; j < filter_btns.length; j++) {
+        filter_btns[j].classList.remove("selected");
       }
 
-      e.target.classList.add('selected');
+      e.target.classList.add("selected");
     }
-  })
+  });
 }
 
 /* CONTACT */
 
-const contact_inputs = document.querySelectorAll('.contact-form input');
+const contact_inputs = document.querySelectorAll(".contact-form input");
 
-for(let i = 0; i < contact_inputs.length; i++) {
-  contact_inputs[i].addEventListener('focusin', (e) => {
-    e.target.parentNode.classList.add('focus');
+for (let i = 0; i < contact_inputs.length; i++) {
+  contact_inputs[i].addEventListener("focusin", (e) => {
+    e.target.parentNode.classList.add("focus");
   });
 
-  contact_inputs[i].addEventListener('keyup', (e) => {
+  contact_inputs[i].addEventListener("keyup", (e) => {
     console.log(e.target.value);
-    e.target.parentNode.classList.add('not-empty');
+    e.target.parentNode.classList.add("not-empty");
   });
 
-  contact_inputs[i].addEventListener('focusout', (e) => {
-    e.target.parentNode.classList.remove('focus');
+  contact_inputs[i].addEventListener("focusout", (e) => {
+    e.target.parentNode.classList.remove("focus");
 
-    if(e.target.value != "") {
-      e.target.parentNode.classList.add('not-empty');
+    if (e.target.value != "") {
+      e.target.parentNode.classList.add("not-empty");
     } else {
-      e.target.parentNode.classList.remove('not-empty');
+      e.target.parentNode.classList.remove("not-empty");
     }
-  })
+  });
 }
