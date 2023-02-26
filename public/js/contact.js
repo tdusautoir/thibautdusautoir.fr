e776f0dc2f269ea33339cf323dc49d443c48c3f9
Thibaut Dusautoir <tdusautoir@myges.fr>
2023-02-26 14:21:31 +0100

    contact - toast animation

--- public/js/contact.js
+++ public/js/contact.js
@@ -70,6 +70,7 @@ if(contact_inputs.length > 0 && contact_submit) {
                 setTimeout(() => {
                     contact_submit.classList.remove('pending');
                     contact_submit.classList.add('success');
+                    toast('success', 'Votre message m\'a bien été envoyé !');
 
                     setTimeout(() => {
                         contact_submit.classList.remove('success');
@@ -91,3 +92,15 @@ window.addEventListener('resize', () => {
         }
     }
 });
+
+function toast(type, message) {
+    document.body.innerHTML += `<div class="toast toast--${type}"><svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg><p>${message}</p></div>`;
+
+    setTimeout(() => {
+        document.querySelector('.toast').classList.add('remove');
+
+        setTimeout(() => {
+            document.querySelector('.toast').remove();
+        }, 600);
+    }, 4000);
+}
